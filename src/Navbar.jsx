6622977b8849bb028db9d9d210f104dad16b913a
Nichole <hotmessmonster87@gmail.com>
2023-07-31 22:10:30 -0600

    trying to fix
    trying to fix router

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,20 +9,23 @@ import Streaming from './components/Streaming'
 export default function Navbar() {
     return (
             <BrowserRouter>
-            <nav className='nav'>
-                <Link to='/' className='site-title'>SPUNK</Link>
-                <Link to='/PlayerCard' className='links'>Player Card</Link>
-                <Link to='/Collections' className='links'>Collections</Link>
-                <Link to='/Streaming' className='links'>Streaming</Link>
-                <Link to='https://www.twitch.tv/' className='links'>Head to TWITCH</Link>
-            </nav>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/PlayerCard" element={<PlayerCard />} />
-            <Route path="/Collections" element={<Collections />} />
-            <Route path="Streaming" element={<Streaming />} />
-        </Routes>
-        </BrowserRouter>
+                <nav className='nav'>
+                    <Link to='/' className='site-title'>SPUNK</Link>
+                    <Link to='/PlayerCard' className='links'>Player Card</Link>
+                    <Link to='/Collections' className='links'>Collections</Link>
+                    <Link to='/Streaming' className='links'>Streaming</Link>
+                    <Link to='https://www.twitch.tv/' className='links'>Head to TWITCH</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/PlayerCard" element={<PlayerCard />} />
+                    <Route path="/Collections" element={<Collections />} />
+                    <Route path="Streaming" element={<Streaming />} />
+                </Routes>
+            </BrowserRouter>
+
+
+
 
     ) 
     
