62022492a8f68af5e44c0d1816afea3046697d42
Nichole <hotmessmonster87@gmail.com>
2023-09-01 00:32:48 -0600

    adjusting responsiveness
    adjusted the text shadows and some of the responsiveness of the site, still need to fix some  of the styling that seemed to break

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,17 +5,20 @@ import PlayerCard from './components/PlayerCard'
 import Collections from './components/Collections'
 import Streaming from './components/Streaming'
 import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Navbar() {
     return (
             <>
-                <nav className='navbar navbar-expand'>
+                <nav className='nav navbar-expand-md'>
+
                     <Link to='/Home' className='site-title'>SPUNK</Link>
                     <Link to='/PlayerCard' className='links'>Player Card</Link>
                     <Link to='/Collections' className='links'>Collections</Link>
                     <Link to='/Streaming' className='links'>Streaming</Link>
                     <Link to='https://www.twitch.tv/' className='links'>Head to TWITCH</Link>
+
                 </nav>
                 <Routes>
                     <Route path="/Home" element={<Home />} />
